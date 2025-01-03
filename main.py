85c5509470616593281d9e35a800f1f2fc3ab2d6
J-Shepherd-Dev <shepstar141@gmail.com>
2025-01-03 17:57:54 +0000

    added groups

--- main.py
+++ main.py
@@ -12,17 +12,23 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # creates player and adds to group above
     while(True):
         #Handle events first
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)  
+        # updates all entities
+        for entity in updatable:
+            entity.update(dt)
         #Fill the screen with black
         screen.fill((0, 0, 0))
-        # Draw the player
-        player.draw(screen)
+        # Draw the player entities
+        for entity in drawable:
+            entity.draw(screen)
         #Update the display
         pygame.display.flip()
         #handle the framerate
